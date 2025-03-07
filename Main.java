3f687f2595bdc12dbd7911b6d9206c9391079320
Charles Müller <62445331+charlesmuller194@users.noreply.github.com>
2025-03-06 21:29:51 -0300

    Adicionar método printHelloPT que imprime "Olá mundo"

--- Main.java
+++ Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public static void printHelloPT() {
+        System.out.println("Olá mundo");
+    } 
 }
