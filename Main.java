ab17ac3419da7df43b7ddfe90f1603145f77e7fb
Charles Müller <62445331+charlesmuller194@users.noreply.github.com>
2025-03-06 21:21:01 -0300

    Adicionar método print que imprime "Hola que tal"

--- Main.java
+++ Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+    public static void print() {
+        System.out.println("Hola que tal");
+    }
+
 }
